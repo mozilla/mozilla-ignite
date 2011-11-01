748a71c34241ede56ed0c31742d0998c258a7bad
David Thompson <mail@fatbusinessman.com>
2011-11-01 17:29:31 +0000

    Add a dummy fixture for the Ignite Challenge.

--- settings_ignite.py
+++ settings_ignite.py
@@ -6,6 +6,8 @@ ROOT_URLCONF = '%s.urls_ignite' % ROOT_PACKAGE
 IGNITE_PROJECT_SLUG = 'us-ignite'
 IGNITE_CHALLENGE_SLUG = 'ignite-challenge'
 
+FIXTURE_DIRS = (path('fixtures'),)
+
 TEMPLATE_DIRS = (path('templates_ignite'),) + TEMPLATE_DIRS
 
 EXCLUDED_MIDDLEWARE = ('commons.middleware.LocaleURLMiddleware',)
