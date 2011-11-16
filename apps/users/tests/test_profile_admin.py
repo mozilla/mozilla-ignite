cf95ba71315c8de8e3d62f3e765daf66abb76d89
Ross Bruniges <rossbruniges@gmail.com>
2011-11-16 16:11:13 +0000

    OK, so now I've got it to 404. But on what and why still non the wiser

--- apps/users/tests/test_profile_admin.py
+++ apps/users/tests/test_profile_admin.py
@@ -21,11 +21,11 @@ class ProfileAdmin(TestCase):
 
     @fudge.patch('django_browserid.auth.BrowserIDBackend.authenticate')
     def test_edit_without_links(self, fake):
-        redirect = '/profile/%s/ % self.User.username'
+        redirect = '/profile/%s/' % self.User.username
         post_data = {
             'name': 'Boozeniges',
             'link_url': 'http://ross-eats.co.uk',
             'link_name': 'ross eats'
         }
         response = self.client.post('/profile/edit/', post_data, follow=True)
-        self.assertRedirects(response, redirect) 
+        self.assertRedirects(response, redirect, status_code=301) 
