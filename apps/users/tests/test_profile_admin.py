a20ef621d42ee98f42bd7c94a4c38ba3cfb8cfed
Ross Bruniges <rossbruniges@gmail.com>
2011-11-17 10:28:29 +0000

    Logging some stuff

--- apps/users/tests/test_profile_admin.py
+++ apps/users/tests/test_profile_admin.py
@@ -1,10 +1,12 @@
 import fudge
+import logging
 
 from django.test import TestCase, Client
-from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 from users.models import Profile, Link
+
+log = logging.getLogger(__name__)
 
 class ProfileAdmin(TestCase):
     
@@ -13,6 +15,7 @@ class ProfileAdmin(TestCase):
         self.User = User.objects.create(
             username=u'test_ross',
             password=u'password2',
+            email=u'ross@test.com',
             is_active=True
         )
         self.profile = Profile.objects.create(
@@ -22,10 +25,12 @@ class ProfileAdmin(TestCase):
     @fudge.patch('django_browserid.auth.BrowserIDBackend.authenticate')
     def test_edit_without_links(self, fake):
         redirect = '/profile/%s/' % self.User.username
+        log.debug(redirect)
         post_data = {
             'name': 'Boozeniges',
             'link_url': 'http://ross-eats.co.uk',
             'link_name': 'ross eats'
         }
         response = self.client.post('/profile/edit/', post_data, follow=True)
+        log.debug(response.redirect_chain)
         self.assertRedirects(response, redirect, status_code=301) 
