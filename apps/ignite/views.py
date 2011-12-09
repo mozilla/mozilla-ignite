389a2481252a7fd222bb5f6ed09a8a135a559791
Ross Bruniges <rossbruniges@gmail.com>
2011-12-09 13:29:51 +0000

    Ensuring that, as expected, is_draft removes submissions from public lists

--- apps/ignite/views.py
+++ apps/ignite/views.py
@@ -9,10 +9,15 @@ def splash(request, project, slug, template_name='challenges/show.html'):
     """Show an individual project challenge."""
     project = get_object_or_404(Project, slug=project)
     challenge = get_object_or_404(project.challenge_set, slug=slug)
+    entries = Submission.objects.filter(
+        phase__challenge=challenge
+    ).exclude(
+        is_draft=True
+    )
     return jingo.render(request, 'ignite/splash.html', {
         'challenge': challenge,
         'project': project,
         'phases': list(enumerate(challenge.phases.all(), start=1)),
-        'entries': Submission.objects.filter(phase__challenge=challenge)[:10],
+        'entries': entries[:10],
         'categories': Category.objects.get_active_categories(),
     })
