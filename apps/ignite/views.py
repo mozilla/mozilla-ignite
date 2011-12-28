b09c5dbfc04a806da86a8b0eefcc03d136786d34
Ross Bruniges <rossbruniges@gmail.com>
2011-12-28 15:42:50 +0000

    Templates now pull in real data

--- apps/ignite/views.py
+++ apps/ignite/views.py
@@ -3,6 +3,7 @@ import jingo
 
 from challenges.models import Submission, Category
 from projects.models import Project
+from blogs.models import BlogEntry
 
 
 def splash(request, project, slug, template_name='challenges/show.html'):
@@ -16,10 +17,15 @@ def splash(request, project, slug, template_name='challenges/show.html'):
     ).extra(
         order_by="?"
     )
+    blogs = BlogEntry.objects.filter(
+        page='splash'
+    )
+
     return jingo.render(request, 'ignite/splash.html', {
         'challenge': challenge,
         'project': project,
         'phases': list(enumerate(challenge.phases.all(), start=1)),
         'entries': entries[:10],
         'categories': Category.objects.get_active_categories(),
+        'blogs': blogs
     })
