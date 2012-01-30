330bf9ab6294d1c7525fa463c3ed32b2f12aad42
David Thompson <mail@fatbusinessman.com>
2012-01-30 12:00:08 +0000

    Fix some failing tests.
    The judge context processor was causing problems on error pages, due to
    the lack of a session. Since the lack of a session means there's no
    point displaying judging information, the processor now fails
    gracefully.

--- apps/challenges/context_processors.py
+++ apps/challenges/context_processors.py
@@ -2,8 +2,14 @@ from challenges.models import JudgeAssignment
 
 def assigned_submissions_processor(request):
     """Add the number of assigned submissions to the request context."""
-    if not request.user.has_perm('challenges.judge_submission'):
+    try:
+        user = request.user
+    except:
+        # For some reason, we can't get hold of the user information
         return {}
-    profile = request.user.get_profile()
+    
+    if not user.has_perm('challenges.judge_submission'):
+        return {}
+    profile = user.get_profile()
     assignment_count = JudgeAssignment.objects.filter(judge=profile).count()
     return {'assignment_count': assignment_count}
