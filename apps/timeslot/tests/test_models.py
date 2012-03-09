cf5490e48b0643ab078731276f4bf3e0b8244a14
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2012-03-09 13:10:03 +0000

    Tests for the current release

--- apps/timeslot/tests/test_models.py
+++ apps/timeslot/tests/test_models.py
@@ -7,7 +7,7 @@ from challenges.models import (Submission, Phase, Challenge, Category,
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.contrib.auth.models import User
-from timeslot.models import TimeSlot
+from timeslot.models import TimeSlot, Release
 from timeslot.tests.fixtures import (create_project, create_challenge,
                                      create_phase, create_user,
                                      create_category, create_submission)
@@ -39,11 +39,36 @@ class TimeSlotModelTest(test_utils.TestCase):
                       TimeSlot, User]:
             model.objects.all().delete()
 
+    def create_release(self, name, is_current, extra_data=None):
+        """Helper to create releases"""
+        data = {'name': name,
+                'is_current': True}
+        if extra_data:
+            data.update(extra_data)
+        return Release.objects.create(**data)
+
+    def test_release_creation(self):
+        """Create a Release with the minimum data"""
+        data = {'name': 'Release', 'is_current': True,}
+        release = Release.objects_create(**data)
+
+    def test_current_release_creation(self):
+        """Test the current release mechanics"""
+        release = self.create_release('Release', True)
+        self.assertEqual(release, Release.objects.get_current())
+        self.assertEqual(Release.objects.filter(is_current=True).count(), 1)
+        # add an extra current release
+        new_release = self.create_release('New release', True)
+        self.assertEqual(new_release, Release.objects.get_current())
+        self.assertEqual(Release.objects.filter(is_current=True).count(), 1)
+
     def create_simple_timeslot(self):
         """Create a ``TimeSlot`` with the less possible data"""
+        release = Release.objects.create(name='Release', is_current=True)
         data = {
             'start_date': datetime.utcnow(),
             'end_date': datetime.utcnow() + relativedelta(hours=1),
+            'release': release,
             }
         timeslot = TimeSlot.objects.create(**data)
         assert timeslot.id, "TimeSlot not created"
