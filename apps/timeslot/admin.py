651588ab70c8c1b3c18f1d140171ae6c6bf969aa
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-03-09 12:53:42 +0000

    Added releases for the timeslots

--- apps/timeslot/admin.py
+++ apps/timeslot/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from timeslot.models import TimeSlot
+from timeslot.models import TimeSlot, Release
 
-admin.site.register(TimeSlot)
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    pass
+
+class ReleaseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_current']
+
+admin.site.register(TimeSlot, TimeSlotAdmin)
+admin.site.register(Release, ReleaseAdmin)
--- apps/timeslot/models.py
+++ apps/timeslot/models.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 
 from django.conf import settings
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 from timeslot.managers import TimeSlotFreeManager
 from timeslot.utils import shorten_object
 
@@ -17,6 +18,7 @@ class TimeSlot(models.Model):
     booking_date = models.DateTimeField(blank=True, null=True)
     webcast_url = models.URLField(verify_exists=False, max_length=500,
                                   blank=True)
+    release = models.ForeignKey('timeslot.Release')
 
     # managers
     objects = models.Manager()
@@ -37,3 +39,19 @@ class TimeSlot(models.Model):
         expire_date = self.booking_date + \
             timedelta(seconds=settings.BOOKING_EXPIRATION)
         return any([expire_date < datetime.now(), self.is_booked])
+
+
+class Release(models.Model):
+    """Each ``TimeSlot`` are part of a release"""
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name')
+    is_current = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """Makes sure we only have a current release"""
+        if self.is_current:
+            self.__class__.objects.update(is_current=False)
+        super(Release, self).save(*args, **kwargs)
