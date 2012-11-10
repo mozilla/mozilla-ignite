9f13ab7e29f0dcf5c821c6398ab5cb793fdee9df
jacobabrennan <jacobabrennan@gmail.com>
2012-11-10 02:26:49 -0500

    Uploaded new WebRTC content for Learning Lab 2.

---
+++ media/ignite/resources/js/webrtc_urls.js
@@ -0,0 +1,8 @@
+ignite.url = {
+    node_linkbox: "../svg/linkbox_padding.svg",
+    node_logo: "../svg/ignite_embossed_logo.svg",
+    logo1: "../svg/ignite_w_text.svg",
+    logo2: "../svg/webrtc_w_text.svg",
+    resource_background: "../svg/ignite_embossed_logo.svg",
+    hackable_demo: undefined, // This should be the only url needed.
+};
