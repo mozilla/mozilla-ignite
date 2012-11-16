5b208a066927088493b31bc5a295d0b0653c7be8
Ross Bruniges <rossbruniges@gmail.com>
2012-11-16 14:04:10 +0000

    Adding in a new resource for the webrtc learning lab

--- media/ignite/resources/js/webrtc_urls.js
+++ media/ignite/resources/js/webrtc_urls.js
@@ -1,8 +1,10 @@
+console.log(ignite_bundle);
+
 ignite.url = {
-    node_linkbox: "../svg/linkbox_padding.svg",
-    node_logo: "../svg/ignite_embossed_logo.svg",
-    logo1: "../svg/ignite_w_text.svg",
-    logo2: "../svg/webrtc_w_text.svg",
-    resource_background: "../svg/ignite_embossed_logo.svg",
+    node_linkbox: ignite_bundle.data.MEDIA_URL + "resources/svg/linkbox_padding.svg",
+    node_logo: ignite_bundle.data.MEDIA_URL + "resources/svg/ignite_embossed_logo.svg",
+    logo1: ignite_bundle.data.MEDIA_URL + "resources/svg/ignite_w_text.svg",
+    logo2: ignite_bundle.data.MEDIA_URL + "resources/svg/webrtc_w_text.svg",
+    resource_background: ignite_bundle.data.MEDIA_URL + "resources/svg/ignite_embossed_logo.svg",
     hackable_demo: "https://github.com/mozilla/mozilla-ignite-learning-lab-demos/tree/master/lab-2-webrtc-video",
 };
