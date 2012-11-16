2c0394a2439c9f45cb06e3a64f5c302739604089
Ross Bruniges <rossbruniges@gmail.com>
2012-11-16 14:58:46 +0000

    40 lashes for checking in a console.log

--- media/ignite/resources/js/webrtc_urls.js
+++ media/ignite/resources/js/webrtc_urls.js
@@ -1,10 +1,8 @@
-console.log(ignite_bundle);
-
 ignite.url = {
     node_linkbox: ignite_bundle.data.MEDIA_URL + "resources/svg/linkbox_padding.svg",
     node_logo: ignite_bundle.data.MEDIA_URL + "resources/svg/ignite_embossed_logo.svg",
     logo1: ignite_bundle.data.MEDIA_URL + "resources/svg/ignite_w_text.svg",
     logo2: ignite_bundle.data.MEDIA_URL + "resources/svg/webrtc_w_text.svg",
     resource_background: ignite_bundle.data.MEDIA_URL + "resources/svg/ignite_embossed_logo.svg",
-    hackable_demo: "https://github.com/mozilla/mozilla-ignite-learning-lab-demos/tree/master/lab-2-webrtc-video",
+    hackable_demo: "https://github.com/mozilla/mozilla-ignite-learning-lab-demos/tree/master/lab-2-webrtc-video"
 };
